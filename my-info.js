c33797a243f9b82944836dd4ff465629386d03f7
Luka <luka.luka.luka93@gmail.com>
2024-12-03 15:08:13 -0800

    finished for home addres and also made a function that will bypass error when daily limit of api has been reached

--- my-info.js
+++ my-info.js
@@ -17,56 +17,82 @@ let myInfo = {
 }
 
 
+
+// Debounce function to limit API calls
+function debounce(func, delay) {
+    let timeout;
+    return function (...args) {
+        clearTimeout(timeout);
+        timeout = setTimeout(() => func.apply(this, args), delay);
+    };
+}
+
 async function geoLocationApi(query) {
     const accessKey = '615062e81c90c97efc8e246aa157e9e1';
     const url = `https://api.positionstack.com/v1/forward?access_key=${accessKey}&query=${encodeURIComponent(query)}&limit=5`;
-    
+
     try {
         const response = await fetch(url);
         const data = await response.json();
-        console.log(data)
-        return data.data; // Array of address objects
+        console.log(data);
+
+        if (data.error) {
+            console.error("API Error:", data.error);
+            return [];
+        }
+
+        return data.data || [];
     } catch (error) {
-        console.error("Error fetching addresses:", error);
+        console.error("Fetch error:", error);
         return [];
     }
 }
-geoLocationApi('909 westbourne drive')
 
-function displayAdresses (addresses) {
+function displayAddresses(addresses) {
     homeAddressesContainer.innerHTML = '';
 
-    if (addresses.length === 0) {
+    if (!addresses || addresses.length === 0) {
         homeAddressesContainer.classList.add('hidden');
         return;
     }
+
     homeAddressesContainer.classList.remove('hidden');
 
-    addresses.forEach(address => {
+    addresses.forEach((address) => {
         const div = document.createElement('div');
         div.classList.add('address-option');
         div.innerHTML = `
-            <h3>${address.name}</h3>
-            <p>${address.locality}${address.region_code}, ${address.postal_code}, ${address.country}</p>
+            <h3>${address.label || "Unnamed Location"}</h3>
+            <p>${address.locality || ""} ${address.region_code || ""}, ${address.postal_code || ""}, ${address.country || ""}</p>
             <hr>
         `;
 
-        div.addEventListener('click', ()=> {
-            homeAddressInput.value = address.name;
-            zipCode.value = address.postal_code;
-            state.value = address.region_code;
+        div.addEventListener('click', () => {
+            homeAddressInput.value = address.label || address.name;
+            zipCode.value = address.postal_code || "";
+            state.value = address.region_code || "";
 
-            myInfo.homeAddress = address.name;
-            myInfo.zipCode = address.postal_code;
-            myInfo.state = address.region_code;
+            myInfo.homeAddress = address.label || address.name;
+            myInfo.zipCode = address.postal_code || "";
+            myInfo.state = address.region_code || "";
 
             homeAddressesContainer.classList.add('hidden');
-        })
+        });
 
         homeAddressesContainer.appendChild(div);
-    })
+    });
 }
 
+const handleInput = debounce(async (e) => {
+    const query = e.target.value.trim();
+    if (query.length > 2) { // Start fetching suggestions after typing 3 characters
+        const addresses = await geoLocationApi(query);
+        displayAddresses(addresses);
+    } else {
+        homeAddressesContainer.classList.add('hidden');
+    }
+}, 300); // 300ms debounce delay
 
+homeAddressInput.addEventListener('input', handleInput);
 
 
