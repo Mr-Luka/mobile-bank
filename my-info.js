6e34a94236a3166fdbb1b1963148b47c73f60e0a
Luka <luka.luka.luka93@gmail.com>
2024-12-03 14:45:00 -0800

    made a function that displays addreses, checks if the address length is equal to 0, if yes, it will hide options container, if not it will show them/ I looped through each addres, for each loop the function creates a div with addres name and below town, zip code and country, and then I made a click event for each option, and with clic it populates some of the inputs automatically, and also it saves it into myInfo object

--- my-info.js
+++ my-info.js
@@ -1,10 +1,11 @@
-const homeAddress = document.querySelector('#home-address');
+const homeAddressInput = document.querySelector('#home-address');
 const zipCode = document.querySelector('#zip-code');
 const state = document.querySelector('#state');
 const aptSuite = document.querySelector('#apt');
 const dob = document.querySelector('#dob');
 const income = document.querySelector('#income');
-const homeAdresses = document.querySelector('.home-address-offer');
+const homeAddressesContainer = document.querySelector('.home-address-offer');
+
 
 let myInfo = {
     homeAddress: '',
@@ -15,29 +16,57 @@ let myInfo = {
     income: '',
 }
 
-async function geoLocationApi (address) {
-    const accessKey = '615062e81c90c97efc8e246aa157e9e1'
-    const url = `https://api.positionstack.com/v1/forward?access_key=${accessKey}&query=${address}`;
-    const response = await fetch(url)
-    const data = await response.json();
-    console.log(data);
-}
-geoLocationApi('909 Westbourne Drive');
 
-
-function captureHomeAddress () {
-    const search = homeAddress.value;
-    if (search.length > 0) {
-        homeAdresses.classList.remove('hidden');
-        geoLocationApi(search);
-    } else {
-        homeAdresses.classList.add('hidden');
+async function geoLocationApi(query) {
+    const accessKey = '615062e81c90c97efc8e246aa157e9e1';
+    const url = `https://api.positionstack.com/v1/forward?access_key=${accessKey}&query=${encodeURIComponent(query)}&limit=5`;
+    
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        console.log(data)
+        return data.data; // Array of address objects
+    } catch (error) {
+        console.error("Error fetching addresses:", error);
+        return [];
     }
-    console.log(search)
+}
+geoLocationApi('909 westbourne drive')
+
+function displayAdresses (addresses) {
+    homeAddressesContainer.innerHTML = '';
+
+    if (addresses.length === 0) {
+        homeAddressesContainer.classList.add('hidden');
+        return;
+    }
+    homeAddressesContainer.classList.remove('hidden');
+
+    addresses.forEach(address => {
+        const div = document.createElement('div');
+        div.classList.add('address-option');
+        div.innerHTML = `
+            <h3>${address.name}</h3>
+            <p>${address.locality}${address.region_code}, ${address.postal_code}, ${address.country}</p>
+            <hr>
+        `;
+
+        div.addEventListener('click', ()=> {
+            homeAddressInput.value = address.name;
+            zipCode.value = address.postal_code;
+            state.value = address.region_code;
+
+            myInfo.homeAddress = address.name;
+            myInfo.zipCode = address.postal_code;
+            myInfo.state = address.region_code;
+
+            homeAddressesContainer.classList.add('hidden');
+        })
+
+        homeAddressesContainer.appendChild(div);
+    })
 }
 
-homeAddress.addEventListener('input', (e)=> {
-    const currentText = e.target.value;
-    console.log(currentText)
-})
+
+
 
