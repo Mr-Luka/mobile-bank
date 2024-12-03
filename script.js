3ee663ec808af02502279a61ffacfdf6fc5e3993
Luka <luka.luka.luka93@gmail.com>
2024-12-02 16:46:50 -0800

    made a function that validates entered letters for name, last name, email and password, and checks if the confirm password matches the first password, and then called the function inside validateRegisterInputs

--- script.js
+++ script.js
@@ -9,6 +9,32 @@ const passwordRegister = document.querySelector('#password-register');
 const confirmPasswordRegister = document.querySelector('#password-register-confirm');
 const submitRegisterButton = document.querySelector('#submit-register');
 
+//created an object that will save the information when we sign-up, so the user can use the info in their account
+let registration = {
+    firstName: '',
+    lastName: '',
+    email: '',
+    password: '',
+}
+
+// Validate if the entered input is equal to the given input character rules
+function validRegisterInput (name, lastName, email, password, passwordMatch){
+  const validNameInput = /^[a-zA-Z\s'-]+$/;
+  const validLastNameInput = /^[a-zA-Z\s'-]+$/;
+  const validEmailInput =  /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  const validPasswordInput = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/;
+
+  if(!validNameInput.test(name) || !validLastNameInput.test(lastName)){
+    alert('Please enter correct letters for your full name')
+  } if (!validEmailInput.test(email)){
+    alert('Please enter a valid email')
+  } if (!validPasswordInput.test(password)){
+    alert("Please enter a valid password, at least 8 characters, 1 uppercase, 1 lowercase, 1 number, 1 special character)")
+  } if (password !== passwordMatch){
+    alert('Passwords do not match')
+  }
+
+}
 
 function validateRegisterInputs () {
 
@@ -16,16 +42,24 @@ function validateRegisterInputs () {
   const lastName = lastNameRegister.value.trim();
   const email = emailRegister.value.trim();
   const password = passwordRegister.value.trim();
+  const confirmPassword = confirmPasswordRegister.value.trim();
 
-  const validEmailInput =  /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-  const validPasswordInput = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/;
-  const validNameInput = /^[a-zA-Z\s'-]+$/;
-  const validLastNameInput = /^[a-zA-Z\s'-]+$/;
-
-
+// Confirm if any of the fields are empty and prompt the message according to the empty field
   if (registerInputs.value.trim() === ''){
     alert('please fill out the register form')
+  } else if (!firstName) {
+    alert('Please enter your first name')
+  } else if(!lastName){
+    alert('Please enter your last name')
+  } else if(!email) {
+    alert('Please enter your email')
+  } else if (!password){
+    alert('Please enter your password')
+  } else if (!confirmPassword) {
+      alert('Please confirm your password')
   }
+// calling a function that checks if the inputs entered are correct based on the set regex rules i set.
+  validRegisterInput(firstName, lastName, email, password, confirmPassword);
 }
 submitRegisterButton.addEventListener('click', validateRegisterInputs)
 
