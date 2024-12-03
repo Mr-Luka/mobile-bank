6018967971b1f2e070edba148b259ddabf4b7cba
Luka <luka.luka.luka93@gmail.com>
2024-12-02 17:40:26 -0800

    finished signin js page, where we just check if the email entered is the same as when we were registering, same with password

--- login.js
+++ login.js
@@ -1,3 +1,24 @@
 const registration = JSON.parse(localStorage.getItem('registration'));
+const logInButton = document.querySelector('#logIn-button');
+const emailInput = document.querySelector('[type="email"]');
+
+function handleLogIn(){
+    const email = document.querySelector('[type="email"]').value;
+    const password = document.querySelector('[type="password"]').value;
+    
+    if (!email){
+        alert('please enter your email')
+    } else if (email !== registration.email){
+        alert('There is no user with that email')
+    } else if (!password){
+        alert('please enter your password')
+    } else if (password !== registration.password){
+        alert('wrong password')
+    } else {
+        location.replace("./myProfile.html")
+    }
+}
+
+logInButton.addEventListener('click', handleLogIn);
 
 console.log(registration)
